bf3f6a1e08187061b3158cdb2c0f89462bf8811e
Pritam Tat <101443828+Pritam12321@users.noreply.github.com>
2022-04-22 22:05:58 +0530

    Delete age.php

--- age.php
+++
@@ -1,9 +0,0 @@
-<?php
-$age=array("X"=>"25","Y"=>"28","Z"=>"30");
-echo "Amit is".$age['X']."Years old";
-foreach($age as $x=>$x_value)
-{
-    echo "key=".$x."value=".$x_value;
-    echo"<br>";
-}
-?>
